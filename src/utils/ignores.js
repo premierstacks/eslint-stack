2e01083ec0003c77cd3704cfa52fb3afed1420a0
Tomáš Chochola <chocholatom1997@gmail.com>
2025-02-27 00:22:42 +0100

    Sync dot files

--- src/utils/ignores.js
+++ src/utils/ignores.js
@@ -11,41 +11,7 @@
  * @see {@link https://github.com/sponsors/tomchochola} GitHub Sponsors
  */
 
-const def = [
-  '**/.DS_Store',
-  '**/.env',
-  '**/.env.backup',
-  '**/.env.development',
-  '**/.env.local',
-  '**/.env.production',
-  '**/.env.staging',
-  '**/.env.testing',
-  '**/.fleet',
-  '**/.idea',
-  '**/.vscode',
-  '**/Homestead.json',
-  '**/Homestead.yaml',
-  '**/auth.json',
-  '**/npm-debug.log',
-  '**/yarn-error.log',
-  '.php-cs-fixer.cache',
-  '.phpunit.cache',
-  '.phpunit.coverage',
-  '.phpunit.result.cache',
-  'assets/vendor',
-  'build',
-  'dist',
-  'node_modules',
-  'public/build',
-  'public/hot',
-  'public/storage',
-  'public/vendor',
-  'resources/vendor',
-  'static/vendor',
-  'storage',
-  'storage/*.key',
-  'vendor',
-];
+const def = ['**/.DS_Store', '**/.fleet', '**/.idea', '**/.vscode', '**/.zed'];
 
 export function ignores(options = def) {
   return [
