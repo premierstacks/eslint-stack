6e24df364c3177a2208b832a52e4c9164f23fa77
Tomáš Chochola <chocholatom1997@gmail.com>
2025-04-21 22:59:16 +0200

    Enhance ESLint configurations for React and TypeScript; add new accessibility and React-specific rules

--- src/browser_typescript_react.js
+++ src/browser_typescript_react.js
@@ -41,6 +41,93 @@ export function createEslintConfigBrowserTypescriptReact() {
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'error',
 
+        'jsx-a11y/anchor-ambiguous-text': 'error',
+        'jsx-a11y/control-has-associated-label': 'error',
+        'jsx-a11y/lang': 'error',
+        'jsx-a11y/no-aria-hidden-on-focusable': 'error',
+        'jsx-a11y/prefer-tag-over-role': 'error',
+
+        'react/boolean-prop-naming': 'error',
+        'react/button-has-type': 'error',
+        'react/checked-requires-onchange-or-readonly': 'error',
+        'react/default-props-match-prop-types': 'error',
+        'react/destructuring-assignment': 'error',
+        // 'react/forbid-component-props': 'error',
+        // 'react/forbid-dom-props': 'error',
+        // 'react/forbid-elements': 'error',
+        'react/forbid-foreign-prop-types': 'error',
+        'react/forbid-prop-types': 'error',
+        'react/forward-ref-uses-ref': 'error',
+        'react/function-component-definition': 'error',
+        'react/hook-use-state': 'error',
+        'react/iframe-missing-sandbox': 'error',
+        'react/jsx-boolean-value': 'error',
+        // 'react/jsx-child-element-spacing': 'error', // prettier
+        // 'react/jsx-closing-bracket-location': 'error', // prettier
+        // 'react/jsx-closing-tag-location': 'error', // prettier
+        'react/jsx-curly-brace-presence': 'error',
+        // 'react/jsx-curly-newline': 'error', // prettier
+        // 'react/jsx-curly-spacing': 'error', // prettier
+        // 'react/jsx-equals-spacing': 'error', // prettier
+        'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+        // 'react/jsx-first-prop-new-line': 'error', // prettier
+        'react/jsx-fragments': 'error',
+        'react/jsx-handler-names': 'error',
+        // 'react/jsx-indent': 'error', // prettier
+        // 'react/jsx-indent-props': 'error', // prettier
+        // 'react/jsx-max-depth': 'error',
+        // 'react/jsx-max-props-per-line': 'error', // prettier
+        // 'react/jsx-newline': 'error', // prettier
+        'react/jsx-no-bind': 'error',
+        'react/jsx-no-constructed-context-values': 'error',
+        'react/jsx-no-leaked-render': 'error',
+        'react/jsx-no-literals': 'error',
+        'react/jsx-no-script-url': 'error',
+        'react/jsx-no-useless-fragment': 'error',
+        // 'react/jsx-one-expression-per-line': 'error', // prettier
+        'react/jsx-pascal-case': 'error',
+        // 'react/jsx-props-no-multi-spaces': 'error', // prettier
+        'react/jsx-props-no-spread-multi': 'error',
+        // 'react/jsx-props-no-spreading': 'error',
+        // 'react/jsx-sort-props': 'error',
+        // 'react/jsx-tag-spacing': 'error', // prettier
+        // 'react/jsx-wrap-multilines': 'error', // prettier
+        'react/no-access-state-in-setstate': 'error',
+        'react/no-adjacent-inline-elements': 'error',
+        'react/no-array-index-key': 'error',
+        'react/no-arrow-function-lifecycle': 'error',
+        'react/no-danger': 'error',
+        'react/no-did-mount-set-state': 'error',
+        'react/no-did-update-set-state': 'error',
+        'react/no-invalid-html-attribute': 'error',
+        // 'react/no-multi-comp': 'error',
+        'react/no-namespace': 'error',
+        'react/no-object-type-as-default-prop': 'error',
+        'react/no-redundant-should-component-update': 'error',
+        // 'react/no-set-state': 'error',
+        'react/no-this-in-sfc': 'error',
+        'react/no-typos': 'error',
+        'react/no-unsafe': 'error',
+        'react/no-unstable-nested-components': 'error',
+        'react/no-unused-class-component-methods': 'error',
+        'react/no-unused-prop-types': 'error',
+        'react/no-unused-state': 'error',
+        'react/no-will-update-set-state': 'error',
+        'react/prefer-es6-class': 'error',
+        'react/prefer-exact-props': 'error',
+        'react/prefer-read-only-props': 'error',
+        'react/prefer-stateless-function': 'error',
+        'react/require-default-props': 'error',
+        'react/require-optimization': 'error',
+        'react/self-closing-comp': 'error',
+        // 'react/sort-comp': 'error',
+        // 'react/sort-default-props': 'error',
+        // 'react/sort-prop-types': 'error',
+        'react/state-in-constructor': 'error',
+        'react/static-property-placement': 'error',
+        'react/style-prop-object': 'error',
+        'react/void-dom-elements-no-children': 'error',
+
         // "class-methods-use-this": "off",
         // "@typescript-eslint/class-methods-use-this": "error", // disadvantages
         // "consistent-return": "off",
